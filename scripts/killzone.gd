31fd9499040145cee2e0176b176a791117db003c
hamshad <hiromega18@gmail.com>
2026-07-18 18:18:58 +0530

    Finished Dying 1.0
    I forgot to set the player collision on 2 and set coin to mask collision
    on 2 from the Pickups section

---
+++ scripts/killzone.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+# I got this by dragging the Timer from the left section
+# and dropped it holding Command
+# The $Timer is the path of the node. For ex. from game to coin = $Player/Coin
+@onready var timer: Timer = $Timer
+
+# The green icon on the left means that this method is Signal
+func _on_body_entered(body: Node2D) -> void:
+	print("You are Dead!")
+	timer.start()
+
+
+# When timer timesout we are reloading the main scene
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
