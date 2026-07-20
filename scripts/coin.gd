a4de4d659502bac45f6480014f1197e73fb7606d
hamshad <hiromega18@gmail.com>
2026-07-20 13:17:18 +0530

    Finished Score

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 # This is Signal function, i can see it in Signals section on the right bars besides Inspector.
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!") # Replace with function body.
+	game_manager.add_point()
 	queue_free() # This removes the coin from the screen
---
+++ scripts/game_manager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Now we know the GameManager is only one in the scene
+# So, we attribute it as Unique so when referencing it from nested scene
+# we can easily import (Its the best practice)
+
+var score: int = 0
+
+@onready var score_label: Label = $ScoreLabel
+
+func add_point():
+	score += 1
+	score_label.text = "You have collected " + str(score) + " Coins"
+	print(score)
