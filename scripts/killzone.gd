26b4949fd8f435a631e062d6ec86840c56d5f07a
hamshad <hiromega18@gmail.com>
2026-07-20 12:17:55 +0530

    Finished Dying 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -8,9 +8,13 @@ extends Area2D
 # The green icon on the left means that this method is Signal
 func _on_body_entered(body: Node2D) -> void:
 	print("You are Dead!")
+	Engine.time_scale = 0.5
+	body.velocity.y = -200.0
+	body.get_node("CollisionShape2D").queue_free() 
 	timer.start()
 
 
 # When timer timesout we are reloading the main scene
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
